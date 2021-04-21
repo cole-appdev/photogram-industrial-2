37d7e625a04e9c86107ea98b4907b278f056df02
johnson-cole <colejohnson@chicagobooth.edu>
2021-04-21 03:46:46 +0000

    Fixed some bugs and added default for user private

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -25,7 +25,7 @@ class Photo < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  has_many :fans, through :likes
+  has_many :fans, through: :likes
 
   validates :caption, presence: true
   validates :image, presence: true
