ba6a18f3ec0af3714c29390c4bd20d028789c694
johnson-cole <colejohnson@chicagobooth.edu>
2021-04-22 04:11:22 +0000

    Finished video 5, not started on remainder of work.

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,14 @@ Rails.application.routes.draw do
   resources :photos
   #resources :users, only: :show
 
-  get "/:username" => "users#show"
+  get ":username/liked" => "photos#liked", as: :liked_photos 
+  get ":username/feed" => "photos#feed", as: :feed_photos
+  #get ":username/followers" => "photos#liked", as: :followers 
+  #get ":username/following" => "photos#liked", as: :liked_photos 
+
+  get "/:username" => "users#show", as: :user
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  
-  
 end
