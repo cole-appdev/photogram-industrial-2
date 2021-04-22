7d3a102ffb4d976027728acc022b402128377aa7
johnson-cole <colejohnson@chicagobooth.edu>
2021-04-22 03:29:49 +0000

    Added commenting

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root "photos#index"
 
-  #get "/users/:id" => "users#show", as: :user
+
   
 
   devise_for :users
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  resources :users, only: :show
+  #resources :users, only: :show
+
+  get "/:username" => "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   
